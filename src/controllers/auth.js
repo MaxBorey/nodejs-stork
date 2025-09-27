f942ef6c66f61022d4da7eccad67b24e85a25cc0
maxborey <maxborey@gmail.com>
2025-09-27 22:10:28 +0300

    google_auth+secure

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -69,15 +69,17 @@ export const getGoogleOAuthUrlController = async (req, res) => {
 };
 
 
-export const loginWithGoogleController = async (req, res) => {
-  const session = await loginOrSignupWithGoogle(req.body.code);
-  setupSession(res, session);
+export const loginWithGoogleController = async (req, res, next) => {
+  try {
+    const session = await loginOrSignupWithGoogle(req.body.code);
+    setupSession(res, session);
 
-  res.json({
-    status: 200,
-    message: 'Successfully logged in via Google OAuth!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully logged in via Google OAuth!',
+      data: { accessToken: session.accessToken },
+    });
+  } catch (e) {
+    next(e);
+  }
 };
