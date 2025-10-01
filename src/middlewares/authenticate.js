8d3beebf1ee2b9ea1df1f6c704ce3b9fded2eebc
maxborey <maxborey@gmail.com>
2025-10-01 22:13:25 +0300

    fiks_middleware_auth

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -5,33 +5,35 @@ import { User } from '../db/models/user.js';
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    throw createHttpError(401, 'Please provide auth header');
+    return next(createHttpError(401, 'Please provide auth header'));
   }
 
   const [bearer, accessToken] = authHeader.split(' ');
 
   if (bearer !== 'Bearer' || !accessToken) {
-    throw createHttpError(401, 'Token should be of type Bearer');
+    return next(createHttpError(401, 'Token should be of type Bearer'));
   }
 
   const session = await Session.findOne({ accessToken });
   if (!session) {
-    throw createHttpError(401, 'Session not found!');
+    return next(createHttpError(401, 'Session not found!'));
   }
 
-  if (session.accessTokenValidUntil < new Date()) {
-    await Session.findByIdAndDelete(session._id);
-    throw createHttpError(401, 'Access token expired!');
+  const isAccessTokenExpired = Date.now() > new Date(session.accessTokenValidUntil).getTime();
+  if (isAccessTokenExpired) {
+    res.set(
+      'WWW-Authenticate',
+      'Bearer realm="api", error="invalid_token", error_description="expired"'
+    );
+    return next(createHttpError(401, 'Access token expired'));
   }
 
   const user = await User.findById(session.userId);
-
   if (!user) {
     await Session.findByIdAndDelete(session._id);
-    throw createHttpError(401, 'User, associated with session, is not found!');
+    return next(createHttpError(401, 'User, associated with session, is not found!'));
   }
 
   req.user = user;
-
   next();
 };
